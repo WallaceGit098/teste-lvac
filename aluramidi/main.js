3671338ae5738d89f1d9162e0885c48a97e1265e
WallaceGit098 <122653428+WallaceGit098@users.noreply.github.com>
2023-02-06 15:59:11 -0300

    Projeto

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,8 +1,16 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom (idDoElementoAudio) {
+    document.querySelector(idDoElementoAudio).play();
 }
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play()
-}
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelector('.tecla_pom')
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+        const tecla = listaDeTeclas[contador];
+        const instrumento= tecla.classList[1];
+        const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+
+    console.log(contador)
+}
