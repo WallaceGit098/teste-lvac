2e3c940aa7b6f1cb272da35fffcce70841da93d2
WallaceGit098 <122653428+WallaceGit098@users.noreply.github.com>
2023-02-06 20:03:20 -0300

    .

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,8 +1,5 @@
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play()
 }
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play()
-}
 
-document.querySelector('.tecla_pom')
+const listaDeTeclas = document.querySelectorAll('.tecla');
