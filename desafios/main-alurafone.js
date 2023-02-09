846b6f0b5a21abd8a0ea4c62a3ff4997feb31969
WallaceGit098 <122653428+WallaceGit098@users.noreply.github.com>
2023-02-08 22:15:13 -0300

    Refazendo para fixação

--- desafios/main-alurafone.js
+++ desafios/main-alurafone.js
@@ -2,20 +2,36 @@ const campo = document.querySelector('input[type=tel]')
 
 const teclado = document.querySelectorAll('.teclado > input')
 
-for (let contador = 0; contador < teclado.length; contador++) {
+/*for (let contador = 0; contador < teclado.length; contador++) {
 
     const tecla = teclado[contador]
 
     tecla.onclick = function () {
         campo.value += tecla.value
     }
+}
+    */
+for (let contador = 0; contador < teclado.length; contador++) {
 
-    tecla.onkeydown = function(evento) {
+    const tecla = teclado[contador]
+    
+    tecla.onclick = function () {
+        campo.value += tecla.value
+        }
+
+/*    tecla.onkeydown = function(evento) {
         if(evento.code === "Enter" || evento.code === "Space"){
             tecla.classList.add('ativa'); 
         }
     }
     tecla.onkeyup = function(){
         tecla.classList.remove('ativa')
-    }
-    
+    }*/
+    tecla.onkeyDown = function(evento){
+        if (evento.code === Enter || evento.code === Space)
+          tecla.classList.add('ativa')
+       }
+       tecla.onkeyUp = function(){
+         tecla.classList.remove('ativa')
+       }
+}
